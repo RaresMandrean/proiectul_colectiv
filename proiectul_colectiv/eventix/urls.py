ff6cb6db4e433a3a4046ab7c4229e35d41e36973
unknown <paulhaiduc27@gmail.com>
2019-12-17 21:18:04 +0200

    added crud for events

--- proiectul_colectiv/eventix/urls.py
+++ proiectul_colectiv/eventix/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
+from .views import (
+    EventListView,
+    EventDetailView,
+    EventCreateView,
+    EventUpdateView,
+    EventDeleteView
+)
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='eventix-home'),
+    path('', EventListView.as_view(), name='eventix-home'),
+    path('event/<int:pk>/', EventDetailView.as_view(), name='event-detail'),
+    path('event/new/', EventCreateView.as_view(), name='event-create'),
+    path('event/<int:pk>/update/', EventUpdateView.as_view(), name='event-update'),
+    path('event/<int:pk>/delete/', EventDeleteView.as_view(), name='event-delete'),
     path('about/', views.about, name='eventix-about'),
 ]
--- proiectul_colectiv/eventix/views.py
+++ proiectul_colectiv/eventix/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 from .models import Event
 
 
@@ -7,6 +15,51 @@ def home(request):
         'events': Event.objects.all()
     }
     return render(request, 'eventix/home.html', context)
+
+class EventListView(ListView):
+    model = Event
+    template_name = 'eventix/home.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'events'
+    ordering = ['-date_posted']
+
+
+class EventDetailView(DetailView):
+    model = Event
+
+
+class EventCreateView(LoginRequiredMixin, CreateView):
+    model = Event
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class EventUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Event
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class EventDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Event
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 
 def about(request):

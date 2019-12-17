957c04551719dc45d2c01e1a80a327ee30df90d0
unknown <paulhaiduc27@gmail.com>
2019-12-17 23:09:44 +0200

    added pagination and some other small changes

--- proiectul_colectiv/eventix/urls.py
+++ proiectul_colectiv/eventix/urls.py
@@ -4,12 +4,14 @@ from .views import (
     EventDetailView,
     EventCreateView,
     EventUpdateView,
-    EventDeleteView
+    EventDeleteView,
+    UserEventListView
 )
 from . import views
 
 urlpatterns = [
     path('', EventListView.as_view(), name='eventix-home'),
+    path('user/<str:username>', UserEventListView.as_view(), name='user-events'),
     path('event/<int:pk>/', EventDetailView.as_view(), name='event-detail'),
     path('event/new/', EventCreateView.as_view(), name='event-create'),
     path('event/<int:pk>/update/', EventUpdateView.as_view(), name='event-update'),
--- proiectul_colectiv/eventix/views.py
+++ proiectul_colectiv/eventix/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 from django.views.generic import (
     ListView,
     DetailView,
@@ -16,11 +17,24 @@ def home(request):
     }
     return render(request, 'eventix/home.html', context)
 
+
 class EventListView(ListView):
     model = Event
     template_name = 'eventix/home.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'events'
     ordering = ['-date_posted']
+    paginate_by = 5
+
+
+class UserEventListView(ListView):
+    model = Event
+    template_name = 'eventix/user_events.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'events'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Event.objects.filter(author=user).order_by('-date_posted')
 
 
 class EventDetailView(DetailView):
